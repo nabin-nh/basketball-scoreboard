8f098a0e0fa2522177fa537c73153aa852ee856e
nabin-nh <nabin.hussain@gmail.com>
2025-11-02 23:21:02 +0000

    Apply increment logic to scoreboard points buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,35 @@
-let passengerBtn = document.getElementById("test")
+let homeCountEl = document.getElementById("home-count")
+let homeCount = 0
 
-passengerBtn.innerText = "Test"
+let guestCountEl = document.getElementById("guest-count")
+let guestCount = 0
+
+function incrementHomeOne() {
+    homeCount += 1
+    homeCountEl.textContent = homeCount
+}
+
+function incrementHomeTwo() {
+    homeCount += 2
+    homeCountEl.textContent = homeCount
+}
+
+function incrementHomeThree() {
+    homeCount += 3
+    homeCountEl.textContent = homeCount
+}
+
+function incrementGuestOne() {
+    guestCount += 1
+    guestCountEl.textContent = guestCount
+}
+
+function incrementGuestTwo() {
+    guestCount += 2
+    guestCountEl.textContent = guestCount
+}
+
+function incrementGuestThree() {
+    guestCount += 3
+    guestCountEl.textContent = guestCount
+}
